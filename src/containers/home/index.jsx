0af94d9b9dec10215d31a8a4b1c54cca5b406d21
ISHWARYAB17 <ishwaryabalaji1700@gmail.com>
2023-08-29 21:53:42 +0530

    This is my portfolio v1

---
+++ src/containers/home/index.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import './styles.scss'
+const Home=() => {
+    return(
+        <section id="home" className="home">
+            <div className="home__text-wrapper">
+                <h1>Hello, I'm Ishwarya
+                    <br/>
+                    Software Engineer
+                </h1>
+
+            </div>
+        </section>
+    )
+}
+export default Home;
